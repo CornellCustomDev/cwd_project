af181a532204a24baa6a18a86f933dbd9c1a3c94
psw58 <psw58@cornell.edu>
2019-09-11 12:15:28 -0400

    fix warning text in search

--- js/project.js
+++ js/project.js
@@ -8,18 +8,18 @@ jQuery(document).ready(function($) {
   $("#cu-search-query").attr("name", "keys");
   $("input[type=radio][name=sitesearch]").on("change", function() {
     switch ($(this).val()) {
-      case "thissite":
+      case "thise":
         $("#cu-search-form").attr("action", "/search/node/");
         $("#cu-search-query").attr("name", "keys");
         $("#cu-search-form").attr("method", "get");
         break;
-      case "cornell":
+      case "cor":
         $("#cu-search-form").attr("action", "//cornell.edu/search/");
         $("#cu-search-query").attr("name", "q");
         $("#cu-search-form").attr("method", "get");
         break;
       default:
-        console.warn($(this).val() + " not found in project search");
+        console.warn($(this).val() + " not implemented.");
     }
   });
 });
