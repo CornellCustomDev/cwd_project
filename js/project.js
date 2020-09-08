fcdce7752ce68dd5a8893f7d34e14e0f79384f01
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2020-09-08 15:28:39 -0400

    hopefully fix search

--- js/project.js
+++ js/project.js
@@ -8,18 +8,18 @@ jQuery(document).ready(function($) {
   $("#cu-search-query").attr("name", "keys");
   $("input[type=radio][name=sitesearch]").on("change", function() {
     switch ($(this).val()) {
-      case "thise":
+      case "thissite":
         $("#cu-search-form").attr("action", "/search/node/");
         $("#cu-search-query").attr("name", "keys");
         $("#cu-search-form").attr("method", "get");
         break;
-      case "cor":
+      case "cornell":
         $("#cu-search-form").attr("action", "//cornell.edu/search/");
         $("#cu-search-query").attr("name", "q");
         $("#cu-search-form").attr("method", "get");
         break;
       default:
-        console.warn($(this).val() + " not implemented.");
+        console.warn($(this).val() + " not found in project search");
     }
   });
 });
